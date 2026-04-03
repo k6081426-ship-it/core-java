c8f2c8f11dca494581dacf9a13b898187434777e
Keerthana T.K <k6081426@gamil.com>
2026-04-03 01:13:59 -0800

    task 02-04-2026

---
+++ Mobile2Runner.java
@@ -0,0 +1,36 @@
+class Mobile2Runner{
+
+    public static void main(String []args){
+	
+	  Mobile m1=new Mobile("Pixel 6","Tensor",2021,"USB-C",90,6.4f);
+	  m1.displayMobileDetails();
+	
+	  Mobile m2=new Mobile("Samsung S21","Exynos 2100",2021,"USB-C",120,6.2f);
+	  m2.displayMobileDetails();
+	 
+	  Mobile m3=new Mobile("OnePlus 10R","Dimensity 8100",2022,"USB-C",120,6.7f);
+	  m3.displayMobileDetails();
+	
+	  Mobile m4=new Mobile("Realme 9 Pro","Snapdragon 695",2022,"USB-C",120,6.6f);
+	  m4.displayMobileDetails();
+	
+	  Mobile m5=new Mobile("Redmi Note 11","Snapdragon 680",2022,"USB-C",90,6.43f);
+	  m5.displayMobileDetails();
+	
+	  Mobile m6=new Mobile("Oppo F19 Pro","Helio P95",2021,"USB-C",60,6.43f);
+      m6.displayMobileDetails();
+	
+	  Mobile m7=new Mobile("Oppo A78","Dimensity 700",2023,"USB-C",90,6.56f);
+      m7.displayMobileDetails();
+	
+	  Mobile m8=new Mobile("Vivo T1","Snapdragon 680",2022,"USB-C",90,6.58f);
+	  m8.displayMobileDetails();
+	
+	  Mobile m9=new Mobile("Nothing Phone 1","Snapdragon 778G+",2022,"USB-C",120,6.55f);
+      m9.displayMobileDetails();
+	
+	  Mobile m10=new Mobile("iQOO Neo 7","Dimensity 8200",2023,"USB-C",120,6.78f);
+      m10.displayMobileDetails();
+	
+	}
+}
---
+++ SmartTv1Runner.java
@@ -0,0 +1,36 @@
+class SmartTv1Runner{
+
+    public static void main(String []args){
+	
+	 SmartTv1 tv1=new SmartTv1("Samsung QLED", 11,64, "Black", "2 Years", "Dolby");
+	 tv1.displayTvDetails();
+	
+	 SmartTv1 tv2=new SmartTv1("LG OLED", 12,67, "Silver", "3 Years", "DTS");
+	 tv2.displayTvDetails();
+	 
+	 SmartTv1 tv3=new SmartTv1("Sony Bravia", 10,62, "Black", "2 Years", "Dolby Atmos");
+	 tv3.displayTvDetails();
+	
+	 SmartTv1 tv4=new SmartTv1("OnePlus TV", 12,34, "Black", "1 Year", "Dolby Audio");
+	 tv4.displayTvDetails();
+	
+	 SmartTv1 tv5=new SmartTv1("Mi TV 5X", 11,78, "Grey", "1 Year", "Stereo");
+	 tv5.displayTvDetails();
+	
+	 SmartTv1 tv6=new SmartTv1("Realme SmartTV", 10,45, "Black", "1 Year", "Surround");
+     tv6.displayTvDetails();
+	
+	 SmartTv1 tv7=new SmartTv1("Panasonic Viera", 9,67, "Dark Grey", "2 Years", "Cinema Sound");
+     tv7.displayTvDetails();
+	
+	 SmartTv1 tv8=new SmartTv1("TCL Android TV", 11,69, "Black", "1 Year", "Dolby");
+	 tv8.displayTvDetails();
+	
+	 SmartTv1 tv9=new SmartTv1("Nokia SmartTV", 11,64, "Black", "2 Years", "JBL Sound");
+     tv9.displayTvDetails();
+	
+	 SmartTv1 tv10=new SmartTv1("Vu Premium", 10,68, "Silver", "1 Year", "Ultra Sound");
+     tv10.displayTvDetails();
+	
+	}
+}
